9224300305ca89fc1eff62b8f2892458ef456779
Paul Ingles <paul@oobaloo.co.uk>
2011-03-08 14:15:08 +0000

    first commit

---
+++ src/clj_rpm/core.clj
@@ -0,0 +1,1 @@
+(ns clj-rpm.core)
---
+++ test/clj_rpm/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-rpm.test.core
+  (:use [clj-rpm.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
